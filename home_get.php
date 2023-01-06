d2f6a6fdd5a2edebb0055ebff9896290d6bf6b44
luisa <121561028+luisatiburcio@users.noreply.github.com>
2023-01-05 21:36:40 -0300

    Add files via upload

---
+++ home_get.php
@@ -0,0 +1,4 @@
+<?php
+    echo "Olá ".$_GET["Luisa"]."<br>";
+    echo "Seu endereço de e-mail é: ".$_GET["luisag@cefet.com.br"]."<br>";
+?>
---
+++ home_post.php
@@ -0,0 +1,4 @@
+<?php 
+	echo "Olá ".$_POST["nome"]."<br>"; 
+	echo "Seu endereço de e-mail é: ".$_POST["email"]."<br>";
+?>
